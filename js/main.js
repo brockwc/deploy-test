7ada97ba2eaeaf518fc218379e59225fea37dcb5
brockwc <brock.whitbread@generalassemb.ly>
2020-02-14 09:12:44 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+console.log("Sanity check!");
+console.log($);
+
+
+// $('a');
+// $('.className');
+// $('#thisIsAnID');
+// $('h1 + p');
+
+$( ".alert" )
+
+$( "#signup-form" )
+
+$( "#blog div" )
+
+$( ".post h2" )
+
+// Yes, you can use CSS pseudo-selectors to select DOM elements!
+$( "div:nth-child(2)" )
+
+// You can also select by HTML attribute.
+// Format: tag + [ attributeName='attributeValue' ]
+// Take note of single quotation marks.
+$( "img[ src='img/5.jpeg' ]" )
